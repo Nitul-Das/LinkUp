d9999d1341bc22d50220d7e7477ad95c028e6e18
Nitul Das <nituldas8753@gmail.com>
2024-10-18 21:35:23 +0530

    auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config()
 const app = express()
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());// to parese req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded) in postman
+app.use(cookieParser()); // it is package installed to get the tokken from cookies in protect routes
 
 
 app.use("/api/auth", routes)
