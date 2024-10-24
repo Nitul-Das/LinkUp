1fe9ebe9448b5f8937fe600403c424d55b6900cd
Nitul Das <nituldas8753@gmail.com>
2024-10-25 00:53:38 +0530

    post routes

---
+++ backend/routes/post.route.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+
+
+import {
+	commentOnPost,
+	createPost,
+	deletePost,
+	getAllPosts,
+	getFollowingPosts,
+	getLikedPosts,
+	getUserPosts,
+	likeUnlikePost,
+} from "../controllers/post.controller.js";
+
+const router = express.Router()
+
+router.post("/create", protectRoute, createPost);
+router.delete("/delete/:id", protectRoute, deletePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+
+
+export default router;
